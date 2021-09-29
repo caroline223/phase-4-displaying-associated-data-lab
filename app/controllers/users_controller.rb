aa65725c8aca4d7b53ffd57ef8286c51e0729f68
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-09-29 00:55:41 -0400

    commit tree 88a7d49c92bb214cb357c6c11a431af081db88db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def show 
+        user = find_user
+        render json: user, include: :items
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
 end
